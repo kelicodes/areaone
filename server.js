c83d1ac44b3d0feb7371fd9d6c360dea6844401c
kelicodes <kelidevske@gmail.com>
2025-08-15 23:33:20 +0300

    cors configuration

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://areaonedaystar.netlify.app",
   credentials: true
 }));
 app.use('/products',productroute)
